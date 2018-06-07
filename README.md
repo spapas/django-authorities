1ea30da5d421a0b7c19776b6adc15be75a5a14d5
Serafeim Papastefanos <spapas@gmail.com>
2018-06-07 13:54:07 +0300

    Add various satellite things

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-authorities
-An application for managing your organization's authorities (departments, directorates etc)
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='django-authorities',
+    version='0.1.0',
+    description="An application for managing your organization's authorities (departments, directorates etc)",
+    long_description=readme(),
+    author='Serafeim Papastefanos',
+    author_email='spapas@gmail.com',
+    license='MIT',
+    url='https://github.com/spapas/django-authorities/',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(exclude=['tests.*', 'tests',]),
+
+    install_requires=['Django >= 1.11', 'six'],
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)

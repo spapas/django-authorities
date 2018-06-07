ef675f29f6c84cd6358ec517ba407d377be5f57b
Serafeim Papastefanos <spapas@gmail.com>
2018-06-07 22:54:51 +0300

    Add template tag

---
+++ authorities/templatetags/authorities_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def user_authorities(context, ):
+    try:
+        return context.request.user.authorities.all()
+    except AttributeError:
+        return []
